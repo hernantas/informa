c5271a9aab8d42f00c13a5edbe56dbcf8c2f0809
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-09-13 15:24:42 +0700

    feat(informa): add `ToTypeResolverFn` function interface

---
+++ packages/informa/src/controller/base/ToTypeResolverFn.ts
@@ -0,0 +1,10 @@
+import { ChangeEvent } from 'react'
+import { DeepPartial } from '../../type/DeepPartial'
+
+/**
+ * Function interface that takes {@link ChangeEvent} of {@link HTMLInputElement}
+ * and convert it into partial `<T>` value
+ */
+export interface ToTypeResolverFn<T> {
+  (event: ChangeEvent<HTMLInputElement>): DeepPartial<T> | undefined
+}
