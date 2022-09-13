997f0e6e271a5e8c0ce0e57ca6bdd20515499ae5
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-09-13 16:00:34 +0700

    docs(informa): tweak `FormFieldGetFn` jsdoc

--- packages/informa/src/controller/field/FormFieldGetFn.ts
+++ packages/informa/src/controller/field/FormFieldGetFn.ts
@@ -1,6 +1,6 @@
 import { DeepPartial } from '../../type/DeepPartial'
 
-/** Function interface that takes property value of `<T>` */
+/** Function interface that return property value of `<T>` */
 export interface FormFieldGetFn<T> {
   <K extends keyof T>(key: K, defaultValue?: DeepPartial<T[K]>):
     | DeepPartial<T[K]>
