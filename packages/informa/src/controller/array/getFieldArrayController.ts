0de5c3e62f8a0c17c37c804dc2e8956520570039
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-09-14 12:13:52 +0700

    feat(informa): add `getFieldArrayController` function

---
+++ packages/informa/src/controller/array/getFieldArrayController.ts
@@ -0,0 +1,70 @@
+import { FormProps } from '../../props/FormProps'
+import { DeepPartial } from '../../type/DeepPartial'
+import { useFormProps } from '../../useFormProps'
+import { FormFieldController } from '../field/FormFieldController'
+import { getFieldController } from '../field/getFieldController'
+import { ArrayModifyFn } from './ArrayInsertFn'
+import { defaultArrayMergeFn } from './defaultArrayMergeFn'
+import { FormFieldArrayController } from './FormFieldArrayController'
+import { FormIndexedFieldController } from './FormIndexedFieldController'
+import { generateKey } from './generateKey'
+
+/**
+ * Generate {@link FormFieldArrayController} from given {@link FormProps}
+ *
+ * @param props
+ * @param id
+ * @returns
+ */
+export function getFieldArrayController<T>(
+  props: FormProps<T[]>,
+  id?: string
+): FormFieldArrayController<T> {
+  const {
+    value,
+    setValue,
+    pass,
+    passComponent,
+    getField,
+    setField,
+    register,
+    registerComponent,
+  } = getFieldController<T[]>(useFormProps(props), defaultArrayMergeFn)
+  const values = value ?? []
+
+  const controllers = values
+    .map<FormProps<T>>((value, index) => ({
+      value,
+      onChange: (newFieldValue) => setField(index, newFieldValue),
+    }))
+    .map<FormFieldController<T>>((props) => getFieldController(props))
+    .map<FormIndexedFieldController<T>>((controller, index) => ({
+      ...controller,
+      index,
+      key: generateKey(index, id),
+    }))
+
+  const append: ArrayModifyFn<T> = (...newValues) =>
+    setValue(values.concat(newValues.length > 0 ? newValues : [undefined]))
+  const prepend: ArrayModifyFn<T> = (...newValues) =>
+    setValue(
+      (newValues.length > 0
+        ? newValues
+        : ([undefined] as (DeepPartial<T> | undefined)[])
+      ).concat(values)
+    )
+
+  return {
+    value,
+    setValue,
+    pass,
+    passComponent,
+    getField,
+    setField,
+    register,
+    registerComponent,
+    controllers,
+    append,
+    prepend,
+  }
+}
