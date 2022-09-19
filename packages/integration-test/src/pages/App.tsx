9a0898dcdfe2763692a47f4c9bc45e2b40d061c5
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-09-19 10:55:26 +0700

    feat(integration-test): add `FieldController` test

--- packages/integration-test/src/pages/App.tsx
+++ packages/integration-test/src/pages/App.tsx
@@ -1,9 +1,11 @@
 import { Controller } from './Controller'
+import { FieldController } from './FieldController'
 
 export function App() {
   return (
     <>
       <Controller />
+      <FieldController />
     </>
   )
 }
---
+++ packages/integration-test/src/pages/FieldController.tsx
@@ -0,0 +1,67 @@
+import { getFieldController, useFormProps } from 'informa'
+import { TestContainer } from '../components/TestContainer'
+
+interface User {
+  id: number
+  username: string
+  password: string
+  active: boolean
+}
+
+export function FieldController() {
+  const props = useFormProps<User>({ value: {} })
+  const controller = getFieldController(props)
+
+  const result = `${props.value?.id}\n${props.value?.username}\n${
+    props.value?.password
+  }\n${props.value?.active ? 'true' : 'false'}`
+  return (
+    <TestContainer title="Field Controller" result={result}>
+      <table>
+        <tr>
+          <td>ID</td>
+          <td>:</td>
+          <td>
+            <input
+              type="text"
+              {...controller.register(
+                'id',
+                (e) => e.target.value as unknown as number
+              )}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Username</td>
+          <td>:</td>
+          <td>
+            <input
+              type="text"
+              {...controller.register('username', (e) => e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Password</td>
+          <td>:</td>
+          <td>
+            <input
+              type="text"
+              {...controller.register('password', (e) => e.target.value)}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>Active</td>
+          <td>:</td>
+          <td>
+            <input
+              type="checkbox"
+              {...controller.register('active', (e) => e.target.checked)}
+            />
+          </td>
+        </tr>
+      </table>
+    </TestContainer>
+  )
+}
