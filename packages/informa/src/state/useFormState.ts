c6dc3ff75aadc2872365a655569f7e4bcfe7c4b5
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-10-03 12:29:31 +0700

    feat(informa): add `useFormState` hooks

---
+++ packages/informa/src/state/useFormState.ts
@@ -0,0 +1,34 @@
+import { useReducer } from 'react'
+import { ChangeFn } from '../props/ChangeFn'
+import { DeepPartial } from '../type/DeepPartial'
+import { CreateSubmit } from './type/CreateSubmit'
+import { FormStateProps } from './FormStateProps'
+import { formStateReducer } from './reducer/formStateReducer'
+
+export function useFormState<T>(
+  initialValue?: DeepPartial<T> | undefined
+): FormStateProps<T> {
+  const [state, setState] = useReducer(formStateReducer, {
+    locked: false,
+    value: initialValue,
+  })
+
+  const onChange: ChangeFn<T> = (newValue) =>
+    setState({ type: 'setValue', value: newValue })
+  const isLocked = () => state.locked
+  const createSubmit: CreateSubmit<T> = (handler) => () => {
+    if (!state.locked) {
+      Promise.resolve(state.value)
+        .finally(() => setState({ type: 'lock' }))
+        .then(handler)
+        .finally(() => setState({ type: 'unlock' }))
+    }
+  }
+
+  return {
+    value: state.value,
+    onChange,
+    isLocked,
+    createSubmit,
+  }
+}
