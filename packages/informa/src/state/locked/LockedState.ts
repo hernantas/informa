e7619bc979b74558ccfcb5fb6a8ff7b8c9e0f870
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2022-12-13 12:22:24 +0700

    refactor(informa): remove unused `LockedState`

--- packages/informa/src/state/locked/LockedState.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from '../../type/Action'
-import { StateCheck } from '../../type/StateCheck'
-
-export interface LockedState {
-  isLocked: StateCheck
-  lock: Action
-  unlock: Action
-}
